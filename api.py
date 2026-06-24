f29868c2b332a7eab6b70801434afb67f4d97e8e
bilalhaider-ux <bilalhaider4911@gmail.com>
2026-06-25 02:06:04 +0500

    Allow wildcard origins in CORS middleware to prevent Vercel client blocks

--- api.py
+++ api.py
@@ -53,17 +53,10 @@ app = FastAPI(
     version="2.0.0",
 )
 
-origins = [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
